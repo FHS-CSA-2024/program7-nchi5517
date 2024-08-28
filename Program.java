cdccdd5e013998a2c09f02dde60cd77226fe95f4
nchi5517 <neelagreev.ch@gmail.com>
2024-08-28 12:09:59 -0700

    Update Program.java
    done with program 7

--- Program.java
+++ Program.java
@@ -1,7 +1,30 @@
 //import stuff here
-
+import java.util.Scanner;
 //Your code here
-
+public class Program7{
+    public static void main(String [] args){
+     double klevin = 0.0;
+     double stanleyNickels = 0.0;
+     double schruteBucks = 0.0;
+     
+     Scanner scan = new Scanner(System.in);
+     System.out.print("Enter # of schrutebucks: ");
+     schruteBucks = scan.nextInt();
+     System.out.print("Enter # of klevins: ");
+     klevin = scan.nextInt();
+     System.out.print("Enter # of stanley-nickels: ");
+     stanleyNickels = scan.nextInt();
+     
+     
+     klevin += stanleyNickels/12.0;
+     schruteBucks += klevin/20.0;
+     schruteBucks = Math.round(100.0*schruteBucks)/100.0;
+     
+     
+     System.out.print("$"+schruteBucks);
+     
+    }
+}
 //Paste console output below:
 /*
 
